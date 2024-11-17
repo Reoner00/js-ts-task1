6795f03aa5bc47100501fafea57f9e1698bba791
Reoner00 <piratrom4@gmail.com>
2024-11-17 18:57:20 +0200

    feat: end-Hometask1

--- src/digitsSum.js
+++ src/digitsSum.js
@@ -4,5 +4,10 @@
  * @returns {number}
  */
 module.exports.digitsSum = function digitsSum(n) {
-  throw new Error('Not implemented'); // remove this line
+  return Math.abs(n)
+    .toString()
+    .split('')
+    .reduce((sum, digits) => sum + Number(digits), 0);
 };
+
+// return Math.abs(n).toString().split('').reduce((sum, digits) =>  sum + Number(digits), 0);
